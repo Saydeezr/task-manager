fcfadb7e25e14b196eedb4faea6a09701616030e
Saydeezr <saydeezr@gmail.com>
2024-04-03 16:09:07 -0400

    debugging

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,8 +4,15 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+    let id = "";
+    for (let i = 0; i < 5; i++) {
+         id += Math.floor(Math.random()*10);
+        
+    }
+      console.log("id", id)
+} 
+generateTaskId();
 
-}
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
@@ -19,6 +26,14 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+   event.preventDefault()
+   const task = {
+    id: generateTaskId(),
+    title : $('#title').val(),
+    date : $('#date').val(),
+    description : $('#description').val(),
+   status: "to-do"
+}
 
 }
 
@@ -36,3 +51,4 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
