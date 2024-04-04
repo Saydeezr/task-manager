35e4225de842ec0c3ee0a05aeabf388d7c8fc735
Saydeezr <saydeezr@gmail.com>
2024-04-04 18:38:17 -0400

    fixed and added functions in JS

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-
+let eachTask = []
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     let id = "";
@@ -11,31 +11,37 @@ function generateTaskId() {
     }
       console.log("id", id)
 } 
-generateTaskId();
+ generateTaskId();
+
+ //what do I want it to do?
+    // 1. on the click of the add task button, I want it to create a task card
+    // 2. made up of: data given by user 
 
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+     const cardDiv = $(`<div>${taskData}</div>`)
 
-}
+    }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+ function renderTaskList(){ //event listener "click"
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+ };
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
    event.preventDefault()
-   const task = {
-    id: generateTaskId(),
+   const taskData = {
+    id : generateTaskId(),
     title : $('#title').val(),
     date : $('#date').val(),
     description : $('#description').val(),
    status: "to-do"
-}
-
-}
+}};
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
