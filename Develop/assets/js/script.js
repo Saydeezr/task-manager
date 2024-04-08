e7088f1aa69b5988e3e14ffb4c5161a013f8dedd
Saydeezr <saydeezr@gmail.com>
2024-04-08 11:41:07 -0400

    fixed some code on a function and added array

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,9 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-let eachTask = ["title", "date","description"];
+const newTask = []
+
+document.getElementById('formModal').addEventListener('submit', handleAddTask);
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -33,24 +35,43 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
  function renderTaskList(){ 
-  for (let i = 0; i < taskList.length; i++) {
-     const givenTask = taskList[i];
-     console.log('givenTask', givenTask)
- }
+    const taskBoard = $('#task-board');
+    taskBoard.empty();
+   
+    taskList.forEach(task => {
+    const taskCard = createTaskCard(task);
+});
+
+    taskCard.draggable({
+    revert: "invalid",
+    cursor: "move"
+});
+    taskBoard.append(taskCard);
  };
 
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-   event.preventDefault()
+   event.preventDefault();
+
+   const title = $('#title').val();
+    const date = $('#date').val();
+    const description = $('#description').val();
+
+    if (!title || !date || !description) {
+        alert('Please fill out all fields');
+        return;
+    }
+
    const task = {
     id : generateTaskId(),
-    title : $('#title').val(),
-    date : $('#date').val(),
-    description : $('#description').val(),
-   status: "to-do"
- 
-}};
+    title : title,
+    date : date,
+    description : description,
+   status: "to-do",
+  };
+  newTask.push(task); 
+};
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
