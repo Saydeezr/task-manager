2c421bb6a288e355f995f79fe27a473feb20a9be
Saydeezr <saydeezr@gmail.com>
2024-04-08 10:22:27 -0400

    fixed task card function

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -9,30 +9,41 @@ function generateTaskId() {
     for (let i = 0; i < 5; i++) {
          id += Math.floor(Math.random()*10);
     }
-      console.log("id", id)
+      return id;
 } 
  generateTaskId();
 
-
+   
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-     const cardDiv = $(`<div>${taskData}</div>`)
+     const cardDiv = $('<div>')
+     .addClass("card w-75 my-3")
+     .attr("data-task-id", task.id);
+
+     const cardHeader =$('<div>').addClass('card-header').text(task.title);
+     const cardBody = $("<div>").addClass("card-body");
+     const description = $('<p>').addClass('card-text').text(task.description);
+     const deadline = $('<p>').addClass('card-text').text('Deadline: ' + task.date);
+ 
+     cardBody.append(description, deadline);
+     cardDiv.append(cardHeader, cardBody);
+     return cardDiv;
     };
 
 
 // Todo: create a function to render the task list and make cards draggable
- function renderTaskList(){ //event listener "click"
-  for (let i = 0; i < eachTask.length; i++) {
-    const task = eachTask[i];
-    console.log('task', task)
-  }
+ function renderTaskList(){ 
+//   for (let i = 0; i < eachTask.length; i++) {
+//     const task = eachTask[i];
+//     console.log('task', task)
+ // }
  };
 
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
    event.preventDefault()
-   const taskData = {
+   const task = {
     id : generateTaskId(),
     title : $('#title').val(),
     date : $('#date').val(),
