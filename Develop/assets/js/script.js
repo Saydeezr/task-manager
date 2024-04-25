aa9acf666b752bbea5aca15f3dddc84d4a0638c6
Saydeezr <saydeezr@gmail.com>
2024-04-25 18:55:17 -0400

    changed the for each to a for loop

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const droppableLane = document.getElementById('.droppable-area')
 const newTask = []
 
 document.getElementById("addTask").addEventListener('click', handleAddTask);
@@ -10,7 +11,7 @@ function generateTaskId() {
     let id = "";
     for (let i = 0; i < 5; i++) {
          id += Math.floor(Math.random()*10);
-    }
+    } console.log(id)
       return id;
 } 
  generateTaskId();
@@ -34,28 +35,31 @@ function createTaskCard(task) {
 
 
 // Todo: create a function to render the task list and make cards draggable
- function renderTaskList(){ 
+function renderTaskList() {
     const taskBoard = $('.task-board');
     taskBoard.empty();
-    if (taskList) { 
-        taskList.forEach (task => { 
-    const taskCard = createTaskCard(task);
-    taskCard.draggable({
-        revert: "invalid",
-        cursor: "move"
-    }); 
-    console.log(taskCard)
-    taskBoard.append(taskCard);
- });
-}};
+
+    if (taskList) {
+        for (let i = 0; i < taskList.length; i++) {
+            const task = taskList[i];
+            const taskCard = createTaskCard(task);
+            taskCard.draggable({
+                revert: "invalid",
+                cursor: "move"
+            });
+            console.log(taskCard);
+            taskBoard.append(taskCard);
+        }
+    }
+}
+
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
    event.preventDefault();
-  console.log('handle click')
    const title = $('#title').val();
-    const date = $('#date').val();
-    const description = $('#description').val();
+   const date = $('#date').val();
+   const description = $('#description').val();
 
     if (!title || !date || !description) {
         alert('Please fill out all fields');
